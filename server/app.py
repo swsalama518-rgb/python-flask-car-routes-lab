8269660e1a7c2466e0aa851dd321dcee060376d6
salma <swsalama518@gmail.com>
2026-06-07 22:44:40 -0400

    update 1

--- server/app.py
+++ server/app.py
@@ -6,14 +6,11 @@ existing_models = ["road", "mountain", "hybrid", "electric"]
 
 @app.route('/')
 def home():
-    return "Welcome to Flatiron Cars!"
+    return "Welcome to Flatiron Cars"
 
 @app.route('/<model>')
 def get_model(model):
     if model in existing_models:
         return f"Flatiron {model} is in our fleet!"
     else:
-        return f"No models called {model} exists in our catalog."
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        return f"No models called {model} exists in our catalog"
